e01b1af9c8bf22f07fd4991f257c8599b51f21f9
Landry <landry.norris@live.com>
2022-10-11 12:23:35 -0500

    Add ability to set loggers for global Analytiks object and delegate logging events

--- analytiks/src/commonMain/kotlin/io/github/landrynorris/analytiks/Analytiks.kt
+++ analytiks/src/commonMain/kotlin/io/github/landrynorris/analytiks/Analytiks.kt
@@ -1,8 +1,22 @@
 package io.github.landrynorris.analytiks
 
+import io.github.landrynorris.analytiks.loggers.PrintLogger
+
 object Analytiks {
+    private val loggers = arrayListOf<AnalytiksLogger>(PrintLogger())
 
-    fun logEvent() {
+    fun setLoggers(vararg newLoggers: AnalytiksLogger) {
+        loggers.clear()
+        addLoggers(*newLoggers)
+    }
 
+    fun addLoggers(vararg newLoggers: AnalytiksLogger) {
+        loggers.addAll(newLoggers)
+    }
+
+    fun logEvent(name: String, extras: Map<String, Any>) {
+        loggers.forEach {
+            it.logEvent(name, extras)
+        }
     }
 }
