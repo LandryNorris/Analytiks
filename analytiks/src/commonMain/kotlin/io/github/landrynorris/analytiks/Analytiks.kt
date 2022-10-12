98b67841d3c03de7e8f5d18bb078f2b0ac2c8dd1
Landry Norris <landry.norris@live.com>
2022-10-12 18:05:17 -0500

    Add docs and default param to logEvent

--- analytiks/src/commonMain/kotlin/io/github/landrynorris/analytiks/Analytiks.kt
+++ analytiks/src/commonMain/kotlin/io/github/landrynorris/analytiks/Analytiks.kt
@@ -5,15 +5,24 @@ import io.github.landrynorris.analytiks.loggers.PrintLogger
 object Analytiks {
     private val loggers = arrayListOf<AnalytiksLogger>(PrintLogger())
 
+    /**
+     * Replace the loggers list with a new list. This removes all existing loggers.
+     */
     fun setLoggers(vararg newLoggers: AnalytiksLogger) {
         loggers.clear()
         addLoggers(*newLoggers)
     }
 
+    /**
+     * Add analytics loggers
+     */
     fun addLoggers(vararg newLoggers: AnalytiksLogger) {
         loggers.addAll(newLoggers)
     }
 
+    /**
+     * Log an event using each analytics logger.
+     */
     fun logEvent(name: String, extras: Map<String, Any> = mapOf()) {
         loggers.forEach {
             it.logEvent(name, extras)
